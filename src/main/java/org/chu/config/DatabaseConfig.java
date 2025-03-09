92dbc892671f535cf78f9ff0a1e39f919b64bee2
Wizardy <wizardhassan99@gmail.com>
2025-03-09 22:59:31 +0000

    database connection implemented with DP Singleton

---
+++ src/main/java/org/chu/config/DatabaseConfig.java
@@ -0,0 +1,40 @@
+package org.chu.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+/**
+ * Configuration de la base de données utilisant le patron Singleton
+ * Spring Boot gère automatiquement le singleton via @Configuration
+ */
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    // Le patron Singleton est géré par Spring via @Bean
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+}
+
